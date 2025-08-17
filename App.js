1278e8403df0168604db8e282df8a5ca21269ddb
Pragmagic <pragmagic@Pragmagics-MacBook-Air.local>
2025-08-17 13:08:48 +0530

    Initial commit - Expo project setup

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: "blue", fontSize: 60}}>Approval Process Manager</Text>
       <StatusBar style="auto" />
     </View>
   );
